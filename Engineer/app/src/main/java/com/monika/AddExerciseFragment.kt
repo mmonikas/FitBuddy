79f3b52c03dea685b36805f2646a80785984adb3
Monika <monika.stobienia@gmail.com>
2019-05-30 00:03:12 +0200

    fragments for adding exercise and fixed layout margins

--- Engineer/app/src/main/java/com/monika/AddExerciseFragment.kt
+++ Engineer/app/src/main/java/com/monika/AddExerciseFragment.kt
@@ -1,19 +1,20 @@
 package com.monika
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-
-class ExerciseDetails : Fragment() {
+class AddExerciseFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_details, container, false)
+        return inflater.inflate(R.layout.fragment_add_exercise, container, false)
     }
 }
